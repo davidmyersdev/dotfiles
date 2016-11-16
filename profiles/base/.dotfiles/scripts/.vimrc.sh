e6d2cd18ebc9af1ef666abfa4acc847d86097339
drm2 <davidrmyersii@gmail.com>
2016-11-16 17:48:24 -0500

    Adding syntax highlighting to vim.

--- profiles/base/.dotfiles/scripts/.vimrc.sh
+++ profiles/base/.dotfiles/scripts/.vimrc.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# download nerdtree if it doesn't exist
+# make sure nerdtree is installed
 if [ ! -d ${HOME}/.vim/bundle/scrooloose/nerdtree ]
 then
     git clone https://github.com/scrooloose/nerdtree ${HOME}/.vim/bundle/scrooloose/nerdtree
+fi
+
+# make sure vim-colorschemes is installed
+if [ ! -d ${HOME}/.vim/bundle/flazz/vim-colorschemes ]
+then
+    git clone https://github.com/flazz/vim-colorschemes ${HOME}/.vim/bundle/flazz/vim-colorschemes
+
+    cp -R ${HOME}/.vim/bundle/flazz/vim-colorschemes/colors ${HOME}/.vim/colors
 fi
--- profiles/base/.vimrc
+++ profiles/base/.vimrc
@@ -3,3 +3,7 @@ filetype off     " required
 
 " set the runtime path to include plugins
 set runtimepath^=$HOME/.vim/bundle/scrooloose/nerdtree
+
+" enable syntax highlighting
+syntax on
+colorscheme wombat
