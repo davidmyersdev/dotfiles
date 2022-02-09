56a57572f665c36f9ffb32ce76e11855897190a3
David R. Myers <davidrmyersii@gmail.com>
2022-02-09 13:29:33 -0500

    Update Hammerspoon window scripts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,60 +1,90 @@
-function center()
-  local app = hs.window.focusedWindow()
-  local appFrame = app:frame()
-  local screen = app:screen()
-  local screenFrame = screen:frame()
+function center(frame)
+  frame.x = (screen().w - frame.w) / 2
+  frame.y = (screen().h - frame.h) / 2
 
-  appFrame.x = (screenFrame.w - appFrame.w) / 2
-  appFrame.y = (screenFrame.h - appFrame.h) / 2
-
-  app:setFrame(appFrame)
+  return frame
 end
 
-function centerLeft()
-  local app = hs.window.focusedWindow()
-  local appFrame = app:frame()
-  local screen = app:screen()
-  local screenFrame = screen:frame()
-
-  appFrame.x = ((screenFrame.w / 4) - appFrame.w) / 2
-  appFrame.y = (screenFrame.h - appFrame.h) / 2
-
-  app:setFrame(appFrame)
+function clearAnimation()
+  hs.window.animationDuration = 0
 end
 
-function centerRight()
-  local app = hs.window.focusedWindow()
-  local appFrame = app:frame()
-  local screen = app:screen()
-  local screenFrame = screen:frame()
+function left(frame)
+  frame.x = ((screen().w / 4) - frame.w) / 2
+  frame.y = (screen().h - frame.h) / 2
 
-  appFrame.x = screenFrame.w - ((screenFrame.w / 8)) - (appFrame.w / 2)
-  appFrame.y = (screenFrame.h - appFrame.h) / 2
-
-  app:setFrame(appFrame)
+  return frame
 end
 
-function resize(width, height)
-  local app = hs.window.focusedWindow()
-  local appFrame = app:frame()
+function resize(frame, dimensions)
+  frame.w = dimensions.w
+  frame.h = dimensions.h
 
-  appFrame.w = width
-  appFrame.h = height
-
-  app:setFrame(appFrame)
+  return frame
 end
 
-hs.hotkey.bind({"control", "option"}, "m", function()
-  resize(1920, 1080)
-  center()
+function right(frame)
+  frame.x = screen().w - ((screen().w / 8)) - (frame.w / 2)
+  frame.y = (screen().h - frame.h) / 2
+
+  return frame
+end
+
+function window(details)
+  local app = hs.window.focusedWindow()
+  local frame = app:frame()
+
+  if details.dimensions ~= nil
+  then
+    frame = resize(frame, details.dimensions)
+  end
+
+  if details.position ~= nil
+  then
+    frame = _G[details.position](frame)
+  end
+
+  clearAnimation()
+  app:setFrame(frame)
+end
+
+function screen()
+  return hs.window.focusedWindow():screen():frame()
+end
+
+hs.hotkey.bind({"control", "option"}, "return", function()
+  window({
+    position = "center",
+    dimensions = screen(),
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "f", function()
-  resize(760, 1080)
-  centerLeft()
+  window({
+    position = "left",
+    dimensions = {
+      w = 760,
+      h = 1080,
+    },
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "l", function()
-  resize(760, 1080)
-  centerRight()
+  window({
+    position = "right",
+    dimensions = {
+      w = 760,
+      h = 1080,
+    },
+  })
+end)
+
+hs.hotkey.bind({"control", "option"}, "m", function()
+  window({
+    position = "center",
+    dimensions = {
+      w = 1920,
+      h = 1080,
+    },
+  })
 end)
