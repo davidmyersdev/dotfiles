c6e1bb14dd16b5d19071e6750fcb4427b72ec2ed
David R. Myers <davidrmyersii@gmail.com>
2023-02-24 18:54:31 -0500

    Add interactive 'fix' alias for 'git commit --fixup'

---
+++ git/aliases/git-fix.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+PS4='' # clear prompt for the set -x call
+set -x # print the subsequent expanded command(s)
+
+git l | fzf --ansi --multi --no-sort --reverse | gsed --expression='s/^[^a-z0-9]*//;/^$/d' | awk '{print $1}' | xargs git commit --fixup
