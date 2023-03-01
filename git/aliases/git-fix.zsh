403e3cbf929f566200f274a91cd77ea1491e36d9
David R. Myers <davidrmyersii@gmail.com>
2023-03-01 15:48:41 -0500

    Show 20 lines by default on 'git fix'

--- git/aliases/git-fix.zsh
+++ git/aliases/git-fix.zsh
@@ -3,4 +3,4 @@
 PS4='' # clear prompt for the set -x call
 set -x # print the subsequent expanded command(s)
 
-git l | fzf --ansi --multi --no-sort --reverse | gsed --expression='s/^[^a-z0-9]*//;/^$/d' | awk '{print $1}' | xargs git commit --fixup
+git l 20 | fzf --ansi --multi --no-sort --reverse | gsed --expression='s/^[^a-z0-9]*//;/^$/d' | awk '{print $1}' | xargs git commit --fixup
