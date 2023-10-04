0d18055db0bfe831378e067caca37e75d324edb9
David R. Myers <davidrmyersii@gmail.com>
2023-10-04 17:24:43 -0400

    Make ultrawide config more flexible

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,26 +6,92 @@ function getScreen()
   return hs.window.focusedWindow():screen():frame()
 end
 
+function centerToTopLeft(details)
+  local center = details.center
+  local size = details.size
+
+  return {
+    x = center.x - (size.w / 2),
+    y = center.y - (size.h / 2),
+  }
+end
+
 function moveWindow(window, details)
+  local size = {
+    w = details.width or window:size().w,
+    h = details.height or window:size().h,
+  }
+
+  local column = details.column or 2.5
+  local columns = details.columns or 4
+  local row = details.row or 1
+  local rows = details.rows or 1
+
+  local columnWidth = getScreen().w / columns
+  local rowHeight = getScreen().h / rows
+
+  local center = {
+    x = (columnWidth * column) - (columnWidth / 2),
+    y = (rowHeight * row) - (rowHeight / 2),
+  }
+
+  local topLeft = centerToTopLeft({
+    center = center,
+    size = size,
+  })
+
+  updateWindow(window, {
+    size = size,
+    topLeft = topLeft,
+  })
+end
+
+function setWindowPosition(window, details)
+  local frame = window:frame()
+
+  frame.x = details.x
+  frame.y = details.y
+
+  hs.window.animationDuration = 0
+
+  window:setFrame(frame)
+end
+
+function setWindowSize(window, details)
   local frame = window:frame()
 
   frame.w = details.width
   frame.h = details.height
 
-  local columns = details.columns or 4
-  local rows = details.rows or 1
-  local column = details.column or 2.5
-  local row = details.row or 1
-
-  local columnWidth = getScreen().w / columns
-  local rowHeight = getScreen().h / rows
-
-  frame.x = (columnWidth * column) - (columnWidth / 2) - (frame.w / 2)
-  frame.y = (rowHeight * row) - (rowHeight / 2) - (frame.h / 2)
-
   hs.window.animationDuration = 0
 
   window:setFrame(frame)
+end
+
+function updateWindow(window, details)
+  local frame = window:frame()
+
+  if details.center then
+    local topLeft = centerToTopLeft({
+      center = details.center,
+      size = details.size or window:size(),
+    })
+
+    frame.x = topLeft.x
+    frame.y = topLeft.y
+  end
+
+  if details.size then
+    frame.w = details.size.w
+    frame.h = details.size.h
+  end
+
+  if details.topLeft then
+    frame.x = details.topLeft.x
+    frame.y = details.topLeft.y
+  end
+
+  window:setFrame(frame, 0)
 end
 
 -- The default grid is 4 columns in a single row. E.g. | 1 | 2 | 3 | 4 |
@@ -67,6 +133,18 @@ hs.hotkey.bind({"control", "option"}, "return", function()
     width = getScreen().w,
     height = getScreen().h,
   })
+end)
+
+hs.hotkey.bind({"control", "shift", "option"}, "return", function()
+  local screen = getScreen()
+
+  for _, window in ipairs(hs.window.allWindows()) do
+    moveWindow(window, {
+      column = 2.5,
+      width = screen.w,
+      height = screen.h,
+    })
+  end
 end)
 
 hs.hotkey.bind({"control", "shift", "option"}, "left", function()
@@ -130,17 +208,41 @@ hs.hotkey.bind({"control", "shift", "option"}, "m", function()
 end)
 
 hs.hotkey.bind({"control", "option"}, "f", function()
-  moveWindow(getCurrentWindow(), {
-    column = 1,
-    width = 760,
-    height = 1080,
+  local screen = getScreen()
+  local columnWidth = (screen.w - 1920) / 2
+
+  local center = {
+    x = (columnWidth / 2),
+    y = screen.h / 2,
+  }
+
+  local size = {
+    w = (columnWidth - 80),
+    h = 1080,
+  }
+
+  updateWindow(getCurrentWindow(), {
+    center = center,
+    size = size,
   })
 end)
 
 hs.hotkey.bind({"control", "option"}, "l", function()
-  moveWindow(getCurrentWindow(), {
-    column = 4,
-    width = 760,
-    height = 1080,
+  local screen = getScreen()
+  local columnWidth = (screen.w - 1920) / 2
+
+  local center = {
+    x = screen.w - (columnWidth / 2),
+    y = screen.h / 2,
+  }
+
+  local size = {
+    w = (columnWidth - 80),
+    h = 1080,
+  }
+
+  updateWindow(getCurrentWindow(), {
+    center = center,
+    size = size,
   })
 end)
