4081978a082d3486b38d0eabb04e9403cf3b0584
David R. Myers <davidrmyersii@gmail.com>
2023-10-04 15:56:46 -0400

    Make it possible to move all windows at once

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,13 @@
-function screen()
+function getCurrentWindow()
+  return hs.window.focusedWindow()
+end
+
+function getScreen()
   return hs.window.focusedWindow():screen():frame()
 end
 
-function window(details)
-  local app = hs.window.focusedWindow()
-  local frame = app:frame()
+function moveWindow(window, details)
+  local frame = window:frame()
 
   frame.w = details.width
   frame.h = details.height
@@ -14,66 +17,130 @@ function window(details)
   local column = details.column or 2.5
   local row = details.row or 1
 
-  local columnWidth = screen().w / columns
-  local rowHeight = screen().h / rows
+  local columnWidth = getScreen().w / columns
+  local rowHeight = getScreen().h / rows
 
   frame.x = (columnWidth * column) - (columnWidth / 2) - (frame.w / 2)
   frame.y = (rowHeight * row) - (rowHeight / 2) - (frame.h / 2)
 
   hs.window.animationDuration = 0
 
-  app:setFrame(frame)
+  window:setFrame(frame)
 end
 
 -- The default grid is 4 columns in a single row. E.g. | 1 | 2 | 3 | 4 |
 hs.hotkey.bind({"control", "option"}, "1", function()
-  window({ column = 1, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 1,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "2", function()
-  window({ column = 2, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 2,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "3", function()
-  window({ column = 3, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 3,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "4", function()
-  window({ column = 4, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 4,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "return", function()
-  window({ column = 2.5, width = screen().w, height = screen().h })
+  moveWindow(getCurrentWindow(), {
+    column = 2.5,
+    width = getScreen().w,
+    height = getScreen().h,
+  })
 end)
 
 hs.hotkey.bind({"control", "shift", "option"}, "left", function()
-  window({ column = 1, columns = 2, width = (screen().w / 2), height = screen().h })
+  moveWindow(getCurrentWindow(), {
+    column = 1,
+    columns = 2,
+    width = (getScreen().w / 2),
+    height = getScreen().h,
+  })
 end)
 
 hs.hotkey.bind({"control", "shift", "option"}, "right", function()
-  window({ column = 2, columns = 2, width = (screen().w / 2), height = screen().h })
+  moveWindow(getCurrentWindow(), {
+    column = 2,
+    columns = 2,
+    width = (getScreen().w / 2),
+    height = getScreen().h,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "left", function()
-  window({ column = 2, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 2,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "right", function()
-  window({ column = 3, width = (screen().w / 4), height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 3,
+    width = (getScreen().w / 4),
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "c", function()
-  window({ column = 2.5, width = 960, height = 540 })
+  moveWindow(getCurrentWindow(), {
+    column = 2.5,
+    width = 960,
+    height = 540,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "m", function()
-  window({ column = 2.5, width = 1920, height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 2.5,
+    width = 1920,
+    height = 1080,
+  })
+end)
+
+hs.hotkey.bind({"control", "shift", "option"}, "m", function()
+  for _, window in ipairs(hs.window.allWindows()) do
+    moveWindow(window, {
+      column = 2.5,
+      width = 1920,
+      height = 1080,
+    })
+  end
 end)
 
 hs.hotkey.bind({"control", "option"}, "f", function()
-  window({ column = 1, width = 760, height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 1,
+    width = 760,
+    height = 1080,
+  })
 end)
 
 hs.hotkey.bind({"control", "option"}, "l", function()
-  window({ column = 4, width = 760, height = 1080 })
+  moveWindow(getCurrentWindow(), {
+    column = 4,
+    width = 760,
+    height = 1080,
+  })
 end)
