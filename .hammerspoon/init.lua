a2c3fe24b973640a96464329e7b606f0ceef2d60
David R. Myers <davidrmyersii@gmail.com>
2025-08-16 17:40:02 -0400

    Add Hammerspoon config changes (quite messy)

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,13 +1,158 @@
+-- Docs
+--
+-- Type annotations:
+--   - https://github.com/LuaLS/lua-language-server/wiki/Annotations
+
+hs.loadSpoon("EmmyLua")
+hs.loadSpoon("ReloadConfiguration")
+
 hs.application.enableSpotlightForNameSearches(true)
+hs.window.animationDuration = 0
+
+spoon.ReloadConfiguration:start()
 
 local blockSize = 50
+local normalHeight = 1080
+local normalWidth = 1920
+-- todo: Add a way to switch between these sizes dynamically...
+local smallerHeight = 945
+local smallerWidth = 1512
+
+-- Active dimensions of center window.
+local currentHeight = normalHeight
+local currentWidth = normalWidth
+
+local hyper = { "command", "control", "option", "shift" }
+local triplet = { "control", "option", "command" }
+local scaler = { "control", "shift", "option" }
+
+hs.hotkey.bind(hyper, "5", function()
+  local timestamp = os.date("%Y-%m-%d-%H-%M-%S")
+
+  hs.window.focusedWindow():snapshot(true):saveToFile("~/Desktop/" .. timestamp .. ".png")
+end)
+
+hs.hotkey.bind(hyper, "t", function()
+  local windows = hs.fnutils.map(hs.window.filter.new():getWindows(), function(win)
+    if win ~= hs.window.focusedWindow() then
+      return {
+        text = win:title(),
+        subText = win:application():title(),
+        image = hs.image.imageFromAppBundle(win:application():bundleID()),
+        id = win:id()
+      }
+    end
+  end)
+
+  local chooser = hs.chooser.new(function(choice)
+    if choice ~= nil then
+      local layout = {}
+      local focused = hs.window.focusedWindow()
+      local toRead  = hs.window.find(choice.id)
+      if hs.eventtap.checkKeyboardModifiers()['alt'] then
+        hs.layout.apply({
+          {nil, focused, focused:screen(), hs.layout.left70, 0, 0},
+          {nil, toRead, focused:screen(), hs.layout.right30, 0, 0}
+        })
+      else
+        hs.layout.apply({
+          {nil, focused, focused:screen(), hs.layout.left50, 0, 0},
+          {nil, toRead, focused:screen(), hs.layout.right50, 0, 0}
+        })
+      end
+      toRead:raise()
+    end
+  end)
+
+  chooser
+    :placeholderText("Choose window for 50/50 split. Hold ⎇ for 70/30.")
+    :searchSubText(true)
+    :choices(windows)
+    :show()
+end)
+
+hs.hotkey.bind(triplet, "1", function()
+  currentHeight = normalHeight
+  currentWidth = normalWidth
+end)
+
+hs.hotkey.bind(triplet, "2", function()
+  currentHeight = smallerHeight
+  currentWidth = smallerWidth
+end)
+
+local centerToTopLeft
+local getCurrentScreen
+local getCurrentScreenFrame
+local getCurrentScreenWindows
+local getCurrentSpaceId
+local getCurrentWindow
+local getScreen
+local moveWindow
+local setWindowPosition
+local setWindowSize
+local updateWindow
+
+--- Derived from: https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-2155567633
+---
+--- @param window hs.window
+local function disableAnimations(window)
+  local app = window:application()
+
+  if not app then
+    print("No application found for window: " .. window:title())
+    return
+  end
+
+  local axApp = hs.axuielement.applicationElement(app)
+
+  if not axApp then
+    print("No AX application found for: " .. app:name())
+    return
+  end
+
+  if axApp.AXEnhancedUserInterface then
+    print("yes: " .. app:name())
+
+    axApp.AXEnhancedUserInterface = false
+  else
+    print("no: " .. app:name())
+  end
+end
+
+
+function getCurrentScreen()
+  return getScreen(getCurrentWindow())
+end
+
+function getCurrentScreenFrame()
+  return getCurrentScreen():frame()
+end
+
+function getCurrentScreenWindows()
+  local currentScreen = getCurrentScreen()
+  local windows = {}
+
+  for _, window in ipairs(hs.window.allWindows()) do
+    if getScreen(window) == currentScreen
+    then
+      table.insert(windows, window)
+    end
+  end
+
+  return windows
+end
+
+function getCurrentSpaceId()
+  return hs.spaces.focusedSpace()
+end
 
 function getCurrentWindow()
   return hs.window.focusedWindow()
 end
 
-function getScreen()
-  return hs.window.focusedWindow():screen():frame()
+function getScreen(window)
+  return window:screen()
 end
 
 function centerToTopLeft(details)
@@ -20,7 +165,10 @@ function centerToTopLeft(details)
   }
 end
 
+---@param window hs.window
 function moveWindow(window, details)
+  disableAnimations(window)
+
   local size = {
     w = details.width or window:size().w,
     h = details.height or window:size().h,
@@ -31,12 +179,13 @@ function moveWindow(window, details)
   local row = details.row or 1
   local rows = details.rows or 1
 
-  local columnWidth = getScreen().w / columns
-  local rowHeight = getScreen().h / rows
+  local screenFrame = getCurrentScreenFrame()
+  local columnWidth = screenFrame.w / columns
+  local rowHeight = screenFrame.h / rows
 
   local center = {
-    x = (columnWidth * column) - (columnWidth / 2),
-    y = (rowHeight * row) - (rowHeight / 2),
+    x = ((columnWidth * column) - (columnWidth / 2) + screenFrame.x),
+    y = ((rowHeight * row) - (rowHeight / 2)) + screenFrame.y,
   }
 
   local topLeft = centerToTopLeft({
@@ -72,6 +221,7 @@ function setWindowSize(window, details)
   window:setFrame(frame)
 end
 
+---@param window hs.window
 function updateWindow(window, details)
   local frame = window:frame()
 
@@ -99,39 +249,63 @@ function updateWindow(window, details)
 end
 
 -- The default grid is 4 columns in a single row. E.g. | 1 | 2 | 3 | 4 |
-hs.hotkey.bind({"control", "option"}, "1", function()
+hs.hotkey.bind(hyper, "1", function()
   hs.spaces.moveWindowToSpace(getCurrentWindow(), 1)
 end)
 
-hs.hotkey.bind({"control", "option"}, "2", function()
+hs.hotkey.bind(hyper, "2", function()
   hs.spaces.moveWindowToSpace(getCurrentWindow(), 2)
 end)
 
-hs.hotkey.bind({"control", "option"}, "3", function()
+hs.hotkey.bind(hyper, "3", function()
   hs.spaces.moveWindowToSpace(getCurrentWindow(), 3)
 end)
 
-hs.hotkey.bind({"control", "option"}, "return", function()
+hs.grid.setMargins(hs.geometry.size(10,10))
+hs.grid.setGrid('8x5')
+
+hs.hotkey.bind(hyper, "g",function()
+  hs.grid.show()
+end)
+
+hs.hotkey.bind(hyper, "r", function()
+  -- Reload the Hammerspoon config.
+  hs.reload()
+end)
+
+-- hs.hotkey.bind(hyper, "z", function()
+--   -- https://github.com/Hammerspoon/hammerspoon/issues/2794#issuecomment-1637453743
+--   for _, app in ipairs(hs.application.runningApplications()) do
+-- 		local ax = hs.axuielement.applicationElement(app)
+-- 		if ax.AXEnhancedUserInterface then
+-- 			print(app:name())
+-- 			ax.AXEnhancedUserInterface = false
+-- 		end
+-- 	end
+-- end)
+
+hs.hotkey.bind(hyper, "return", function()
   moveWindow(getCurrentWindow(), {
     column = 2.5,
-    width = getScreen().w,
-    height = getScreen().h,
+    width = getCurrentScreenFrame().w,
+    height = getCurrentScreenFrame().h,
   })
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "return", function()
-  local screen = getScreen()
+hs.hotkey.bind(scaler, "return", function()
+  local screenFrame = getCurrentScreenFrame()
+  local windows = getCurrentScreenWindows()
 
-  for _, window in ipairs(hs.window.allWindows()) do
+  for _, window in ipairs(windows) do
     moveWindow(window, {
       column = 2.5,
-      width = screen.w,
-      height = screen.h,
+      width = screenFrame.w,
+      height = screenFrame.h,
     })
   end
 end)
 
-hs.hotkey.bind({"control", "option"}, "c", function()
+hs.hotkey.bind(hyper, "c", function()
   moveWindow(getCurrentWindow(), {
     column = 2.5,
     width = 960,
@@ -139,69 +313,77 @@ hs.hotkey.bind({"control", "option"}, "c", function()
   })
 end)
 
-hs.hotkey.bind({"control", "option"}, "m", function()
+hs.hotkey.bind(hyper, "m", function()
   moveWindow(getCurrentWindow(), {
     column = 2.5,
-    width = 1920,
-    height = 1080,
+    width = currentWidth,
+    height = currentHeight,
   })
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "m", function()
-  for _, window in ipairs(hs.window.allWindows()) do
+hs.hotkey.bind(hyper, "i", function()
+  local screen = getCurrentScreen()
+  local window = getCurrentWindow()
+
+  print("screen.w: ", screen:frame().w)
+  print("screen.h: ", screen:frame().h)
+  print("screen.x: ", screen:frame().x)
+  print("screen.y: ", screen:frame().y)
+  print("window.w: ", window:size().w)
+  print("window.h: ", window:size().h)
+  print("window.x: ", window:frame().x)
+  print("window.y: ", window:frame().y)
+end)
+
+hs.hotkey.bind(scaler, "m", function()
+  local windows = getCurrentScreenWindows()
+
+  for _, window in ipairs(windows) do
     moveWindow(window, {
       column = 2.5,
-      width = 1920,
-      height = 1080,
+      width = currentWidth,
+      height = currentHeight,
     })
   end
 end)
 
-hs.hotkey.bind({"control", "option"}, "f", function()
-  local screen = getScreen()
+hs.hotkey.bind(scaler, "s", function()
+  hs.window.setShadows(false)
+end)
+
+hs.hotkey.bind(hyper, "f", function()
+  local screen = getCurrentScreenFrame()
   local columnWidth = (screen.w - 1920) / 2
+  local window = getCurrentWindow()
 
-  local center = {
-    x = (columnWidth / 2),
-    y = screen.h / 2,
-  }
+  -- hs.layout.apply({
+  --   { nil, window, window:screen(), hs.layout.left70, nil, nil },
+  -- })
 
-  local size = {
-    w = (columnWidth - 160),
-    h = 1080,
-  }
-
-  updateWindow(getCurrentWindow(), {
-    center = center,
-    size = size,
+  moveWindow(window, {
+    column = 1,
+    width = (columnWidth - 160),
+    height = currentHeight,
   })
 end)
 
-hs.hotkey.bind({"control", "option"}, "l", function()
-  local screen = getScreen()
+hs.hotkey.bind(hyper, "l", function()
+  local screen = getCurrentScreenFrame()
   local columnWidth = (screen.w - 1920) / 2
+  local window = getCurrentWindow()
 
-  local center = {
-    x = screen.w - (columnWidth / 2),
-    y = screen.h / 2,
-  }
-
-  local size = {
-    w = (columnWidth - 160),
-    h = 1080,
-  }
-
-  updateWindow(getCurrentWindow(), {
-    center = center,
-    size = size,
+  moveWindow(window, {
+    column = 4,
+    width = (columnWidth - 160),
+    height = currentHeight,
   })
 end)
 
-hs.hotkey.bind({"control", "option"}, "n", function()
+hs.hotkey.bind(hyper, "n", function()
   hs.application.open('Octo')
 end)
 
-hs.hotkey.bind({"control", "option"}, "up", function()
+hs.hotkey.bind(hyper, "up", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -210,7 +392,7 @@ hs.hotkey.bind({"control", "option"}, "up", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "option"}, "down", function()
+hs.hotkey.bind(hyper, "down", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -220,7 +402,7 @@ hs.hotkey.bind({"control", "option"}, "down", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "left", function()
+hs.hotkey.bind(scaler, "left", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -229,7 +411,7 @@ hs.hotkey.bind({"control", "shift", "option"}, "left", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "right", function()
+hs.hotkey.bind(scaler, "right", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -238,7 +420,7 @@ hs.hotkey.bind({"control", "shift", "option"}, "right", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "down", function()
+hs.hotkey.bind(scaler, "down", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -247,7 +429,7 @@ hs.hotkey.bind({"control", "shift", "option"}, "down", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "shift", "option"}, "up", function()
+hs.hotkey.bind(scaler, "up", function()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -256,8 +438,56 @@ hs.hotkey.bind({"control", "shift", "option"}, "up", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "option"}, "left", function()
-  local screen = getScreen()
+hs.hotkey.bind(triplet, "left", function()
+  local screen = getCurrentScreenFrame()
+  local window = getCurrentWindow()
+  local frame = window:frame()
+
+  frame.x = screen.x
+  frame.y = screen.y
+
+  frame.h = screen.h
+  frame.w = screen.w / 2
+
+  print("frame.x: ", frame.x)
+  print("frame.y: ", frame.y)
+  print("frame.w: ", frame.w)
+  print("frame.h: ", frame.h)
+
+  print("screen.x: ", screen.x)
+  print("screen.y: ", screen.y)
+  print("screen.w: ", screen.w)
+  print("screen.h: ", screen.h)
+
+  window:setFrame(frame, 0)
+end)
+
+hs.hotkey.bind(triplet, "right", function()
+  local screen = getCurrentScreenFrame()
+  local window = getCurrentWindow()
+  local frame = window:frame()
+
+  frame.x = screen.w / 2
+  frame.y = screen.y
+
+  frame.h = screen.h
+  frame.w = screen.w / 2
+
+  print("frame.x: ", frame.x)
+  print("frame.y: ", frame.y)
+  print("frame.w: ", frame.w)
+  print("frame.h: ", frame.h)
+
+  print("screen.x: ", screen.x)
+  print("screen.y: ", screen.y)
+  print("screen.w: ", screen.w)
+  print("screen.h: ", screen.h)
+
+  window:setFrame(frame, 0)
+end)
+
+hs.hotkey.bind(hyper, "left", function()
+  local screen = getCurrentScreenFrame()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -266,8 +496,8 @@ hs.hotkey.bind({"control", "option"}, "left", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "option"}, "right", function()
-  local screen = getScreen()
+hs.hotkey.bind(hyper, "right", function()
+  local screen = getCurrentScreenFrame()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -276,8 +506,8 @@ hs.hotkey.bind({"control", "option"}, "right", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "option"}, "down", function()
-  local screen = getScreen()
+hs.hotkey.bind(hyper, "down", function()
+  local screen = getCurrentScreenFrame()
   local window = getCurrentWindow()
   local frame = window:frame()
 
@@ -286,8 +516,8 @@ hs.hotkey.bind({"control", "option"}, "down", function()
   window:setFrame(frame, 0)
 end)
 
-hs.hotkey.bind({"control", "option"}, "up", function()
-  local screen = getScreen()
+hs.hotkey.bind(hyper, "up", function()
+  local screen = getCurrentScreenFrame()
   local window = getCurrentWindow()
   local frame = window:frame()
 
